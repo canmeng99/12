1bd9b8d102034b40b024e67a85ad8a33decc70c3
canmeng99 <82589539+canmeng99@users.noreply.github.com>
2021-05-03 19:33:12 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/BCYDTZ/luci-app-UUGameAcc.git
-#git clone --depth 1 https://github.com/fw876/helloworld.git
-#mv ./helloworld/luci-app-ssr-plus .
-#rm -rf helloworld
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/fw876/helloworld.git
+rm -rf ./*/.git
+mv ./helloworld/luci-app-ssr-plus .
+rm -rf helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+rm -rf ./*/.git
 mv ./openwrt-passwall/* .
 rm -rf openwrt-passwall
-git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome.git
-git clone --depth 1 https://github.com/small-5/luci-app-xlnetacc.git
+git clone https://github.com/kongfl888/luci-app-adguardhome.git
+git clone https://github.com/small-5/luci-app-xlnetacc.git
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
 

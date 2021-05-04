40664d7330c4203fa3b97e1bc16aa313d3aabaf0
canmeng99 <82589539+canmeng99@users.noreply.github.com>
2021-05-04 19:32:16 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -12,21 +12,21 @@ rm -rf ./*/.git
 mv ./openwrt-passwall/* .
 rm -rf openwrt-passwall
 
-git clone https://github.com/garypang13/luci-app-dnsfilter.git
+#git clone https://github.com/garypang13/luci-app-dnsfilter.git
 #git clone https://github.com/tty228/luci-app-serverchan.git
-git clone https://github.com/sirpdboy/luci-app-netdata.git
+#git clone https://github.com/sirpdboy/luci-app-netdata.git
 #原作者
 #git clone https://github.com/kongfl888/luci-app-adguardhome.git
 #git clone https://github.com/kongfl888/luci-app-adguardhome
 # vssr
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
+#git clone https://github.com/jerrykuku/luci-app-vssr.git
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
 # 迅雷快鸟
 git clone https://github.com/small-5/luci-app-xlnetacc.git
 # 主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
 git clone https://github.com/garypang13/luci-theme-edge.git
 # AdGuardHome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/AdGuardHome
